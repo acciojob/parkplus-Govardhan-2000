e2db4090f75ced35d26d35729bd6ad59e7a5c60f
Govardhan <kurubagovardhan32@gmail.com>
2023-10-04 21:04:05 +0530

    code

--- src/main/java/com/driver/services/impl/UserServiceImpl.java
+++ src/main/java/com/driver/services/impl/UserServiceImpl.java
@@ -1,29 +1,9 @@
 package com.driver.services.impl;
+import com.driver.model.User;
 
-import com.driver.repository.UserRepository;
-import com.driver.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+public interface UserService {
 
-import java.util.ArrayList;
-
-@Service
-public class UserServiceImpl implements UserService {
-
-    @Autowired
-    UserRepository userRepository4;
-    @Override
-    public void deleteUser(Integer userId) {
-
-    }
-
-    @Override
-    public User updatePassword(Integer userId, String password) {
-
-    }
-
-    @Override
-    public void register(String name, String phoneNumber, String password) {
-
-    }
+    void deleteUser(Integer userId);
+    User updatePassword(Integer userId, String password);
+    void register(String name, String phoneNumber, String password);
 }
